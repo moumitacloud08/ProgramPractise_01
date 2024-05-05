31908a43a799de68cad5c8e3c6506e49e03c1ede
moumitacloud08 <moumitacloud08@gmail.com>
2024-05-05 02:48:51 -0700

    Create Nested class Object outside of outer class

---
+++ src/main/java/org/example/innerclass/Inner_05_Runner.java
@@ -0,0 +1,12 @@
+package org.example.innerclass;
+
+/**
+ * Create Nested class Object outside
+ * of outer class
+ */
+public class Inner_05_Runner {
+    public static void main(String[] args){
+        Outer_05.Inner_05 i = new Outer_05.Inner_05();
+        i.m1();
+    }
+}
