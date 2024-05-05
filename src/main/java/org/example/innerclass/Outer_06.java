79f6498b011d71cbafd0a1d59d42f81dad745254
moumitacloud08 <moumitacloud08@gmail.com>
2024-05-05 03:06:40 -0700

    static nested class

---
+++ src/main/java/org/example/innerclass/Outer_06.java
@@ -0,0 +1,15 @@
+package org.example.innerclass;
+
+/**
+ * static nested class
+ */
+public class Outer_06 {
+    static class Inner_06{
+        public static void main(String[] args){
+            System.out.println("Static Nested Class main method ");
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Outer Class main method ");
+    }
+}
