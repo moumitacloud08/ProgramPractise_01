e82a3bf1a0c1cf2c2d82a23ca1eb3381a0d4f105
moumitacloud08 <moumitacloud08@gmail.com>
2025-07-11 10:32:58 -0700

    jdk10

---
+++ src/main/java/org/java/version/jdk10/LocalVariableTypeRef.java
@@ -0,0 +1,12 @@
+package org.java.version.jdk10;
+
+public class LocalVariableTypeRef {
+
+    protected void example1(){
+        var message = "Hello, Java 10";
+        var number = 10;
+
+        System.out.println(message);
+        System.out.println(number);
+    }
+}
---
+++ src/main/java/org/java/version/jdk10/Solution.java
@@ -0,0 +1,8 @@
+package org.java.version.jdk10;
+
+public class Solution {
+    public static void main(String[] args){
+        LocalVariableTypeRef ref = new LocalVariableTypeRef();
+        ref.example1();
+    }
+}
