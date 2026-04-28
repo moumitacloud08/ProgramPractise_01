214feddf837da18335732ee092ff941088d43e16
Moumita Ray <moumitacloud08@gmail.com>
2026-04-28 11:43:21 +0530

    critical section

---
+++ src/main/java/org/example/thread/resourcesharing/criticalsection/synchronizedlock/Main.java
@@ -0,0 +1,79 @@
+package org.example.thread.resourcesharing.criticalsection.synchronizedlock;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+
+    }
+
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+
+    }
+
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+
+    }
+
+    private static class InventoryCounter {
+        private int items = 0;
+
+        final Object lock = new Object();
+
+        public synchronized void increment() {
+            synchronized (this.lock){
+                items++;
+            }
+
+        }
+
+        public synchronized void decrement() {
+            synchronized (this.lock){
+                items--;
+            }
+
+        }
+
+        public synchronized int getItems() {
+            synchronized (this.lock){
+                return items;
+            }
+
+        }
+    }
+
+}
