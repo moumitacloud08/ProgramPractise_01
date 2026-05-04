4d59ab7b6ff678a3fc38652d2ce2835178e61b1c
Moumita Ray <moumitacloud08@gmail.com>
2026-05-04 15:35:11 +0530

    semaphore

---
+++ src/main/java/org/example/thread/semaphore/App.java
@@ -0,0 +1,48 @@
+package org.example.thread.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+enum Downloader {
+
+    INSTANCE;
+    private Semaphore semaphore = new Semaphore(3, true);
+
+    public void download() {
+        try {
+            semaphore.acquire();
+            downloadData();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private void downloadData() {
+        try {
+            System.out.println("Downloading data from the web.....");
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 12; i++) {
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+                    Downloader.INSTANCE.download();
+                }
+            });
+        }
+    }
+
+}
