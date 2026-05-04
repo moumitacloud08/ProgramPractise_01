23790b680463f32b5a9a59c4ebcc00c4d55365c6
Moumita Ray <moumitacloud08@gmail.com>
2026-05-04 14:39:55 +0530

    deadlock

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/moumitacloud08/ProgramPractise_01.git
 git push -u origin main
+-----------------------
+
+new Thread(deadlock:: worker1,"worker1").start();
+
+new Thread(new Runnable() {
+    @Override
+    public void run() {
+      deadlock.worker1();
+    }
+},"worker1").start();
---
+++ src/main/java/org/example/thread/deadlock/case3/Deadlock.java
@@ -0,0 +1,48 @@
+package org.example.thread.deadlock.case3;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Deadlock {
+
+    private Lock lock1 = new ReentrantLock();
+    private Lock lock2 = new ReentrantLock();
+
+    public static void main(String[] args) {
+        Deadlock deadlock = new Deadlock();
+
+        new Thread(deadlock:: worker1,"worker1").start();
+        new Thread(deadlock:: worker2,"worker2").start();
+
+        //With Runnable - ReadMe
+    }
+
+    public void worker1() {
+        lock1.lock();
+        System.out.println("Worker1 acquires the lock1....");
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        lock2.lock();
+        System.out.println("Worker1 acquires the lock2....");
+        lock1.unlock();
+        lock2.unlock();
+    }
+
+    public void worker2() {
+        lock2.lock();
+        System.out.println("Worker2 acquires the lock2...");
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        lock1.lock();
+        System.out.println("Worker2 acquires the lock1....");
+        lock1.unlock();
+        lock2.unlock();
+    }
+
+}
